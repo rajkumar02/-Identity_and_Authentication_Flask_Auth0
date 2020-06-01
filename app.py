56483b64c07330b0e65d4bdf8e6e23ee2cebe540
Raj Kumar Barmon <32005309+rajkumar02@users.noreply.github.com>
2020-05-31 20:06:57 -0700

    restricted limiting features by api permission

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from urllib.request import urlopen
 
 app = Flask(__name__)
 
-AUTH0_DOMAIN = @TODO_REPLACE_WITH_YOUR_DOMAIN #Your Auth0 host url
+AUTH0_DOMAIN =  #@TODO_REPLACE_WITH_YOUR_DOMAIN #Your Auth0 host url
 ALGORITHMS = ['RS256']
-API_AUDIENCE = @TODO_REPLACE_WITH_YOUR_API_AUDIENCE #Your API Audience ID
+API_AUDIENCE = #@TODO_REPLACE_WITH_YOUR_API_AUDIENCE
 
 class AuthError(Exception):
     def __int__(self, error, status_code):
@@ -116,33 +116,62 @@ def get_token_auth_header():
         abort(401)
     return header_path[1]
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
-    return wrapper
+def check_permission(permission, payload):
+    if 'permissions' not in payload:
+        raise AuthError({
+            'code': 'invalid_claims',
+            'description': 'Permisson not included in jwt'
+         }, 400)
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permisson not found'
+        }, 403)
+    return True
 
-@app.route('/headers')
-@requires_auth
-def headers(payload):
-    print(payload)
-    return 'Access Granted!'
+def requires_auth(permission=''):
+    def requires_auth_decoretor(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
+            
+            check_permission(permission, payload)
+
+            return f(payload, *args, **kwargs)
+        return wrapper
+    return requires_auth_decoretor
+
+# @app.route('/headers')
+# @requires_auth
+# def headers(payload):
+#     print(payload)
+#     return 'Access Granted!'
 
 @app.route('/images')
-@requires_auth
+@requires_auth('get/images') #Add your custom permission by  creating Auth0
 def images(jwt):
     print(jwt)
-    return 'not implemented.'
+    return 'Permission granted!'
 
-@app.route('/')
-def index():
-    return f"<h1>Welcome to Index!!</h1>"
+# @app.route('/')
+# def index():
+#     return f"<h1>Welcome to Index!!</h1>"
 
 
 # if __name__ == '__main__':
 #     app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
+
+#Javascript to decode JWT:
+# function parseJwt (token) {
+#     // https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript
+#    var base64Url = token.split('.')[1];
+#    var base64 = decodeURIComponent(atob(base64Url).split('').map((c)=>{
+#        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+#    }).join(''));
+
+#    return JSON.parse(base64);
+# };
